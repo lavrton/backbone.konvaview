d2de2aac737d5b02ae9db661336072babbe0a703
Лаврёнов Антон <lavrton@gmail.com>
2014-05-18 16:47:57 +0800

    updating API (closer to native Backbone.View)

--- backbone.KineticView.js
+++ backbone.KineticView.js
@@ -5,6 +5,8 @@
       Backbone.KineticView = factory(root, Backbone, Kinetic);
       return Backbone;
     });
+  } else if (typeof exports !== 'undefined') {
+    factory(root, require('backbone'), require('kinetic'));
   } else {
     root.Backbone.KineticView = factory(root, Backbone, Kinetic);
   }
@@ -12,36 +14,39 @@
 }(this, function(root, Backbone, Kinetic) {
 
      var KineticView = function(options) {
-      options = options || {};
       this.cid = _.uniqueId('view');
-      this._configure(options || {});
+      options || (options = {});
+      _.extend(this, _.pick(options, viewOptions));
       this._ensureElement();
-      this.delegateEvents();
       this.initialize.apply(this, arguments);
     };
     KineticView.extend = Backbone.Model.extend;
     var delegateEventSplitter = /^(\S+)\s*(.*)$/;
-    var viewOptions = ['model', 'collection', 'el', 'attributes', 'events'];
+    var viewOptions = ['model', 'collection', 'el', 'id', 'attributes', 'events', 'nodeType'];
 
     _.extend(KineticView.prototype, Backbone.Events, {
+      nodeType : 'Group',
+
       initialize: function(){},
-      el : function(){
-        return new Kinetic.Group();
-      },
+
       render : function(){
         return this;
       },
+
       remove: function() {
-        this.$el.destroy();
+        this.undelegateEvents();
+        this.el.destroy();
         this.stopListening();
         return this;
       },
-      setElement: function(element, delegate) {
-        if (this.$el) this.undelegateEvents();
-        this.$el = element;
-        if (delegate !== false) this.delegateEvents();
+
+      setElement: function(element) {
+        this.undelegateEvents();
+        this.el = element;
+        this.delegateEvents();
         return this;
       },
+
       _deleteEventFromNode : function(node) {
         var _this = this;
         _.each(_.keys(node.eventListeners),function(eventType){
@@ -50,11 +55,24 @@
       },
       undelegateEvents: function() {
         var _this = this;
-        this._deleteEventFromNode(this.$el);
-        _.each(this.$el.children,function(child){
+        if (!this.el) {
+          return this;
+        }
+        this._deleteEventFromNode(this.el);
+        _.each(this.el.children, function(child){
           _this._deleteEventFromNode(child);
         });
         return this;
+      },
+      undelegate: function(eventName, selector) {
+        eventName += '.delegateEvents' + this.cid;
+        if (selector === '' || !selector) {
+          this.el.off(eventName);
+        } else {
+          this.el.find && this.el.find(selector).each(function(child){
+            child.off(eventName);
+          });
+        }
       },
       delegateEvents: function(events) {
         if (!(events || (events = _.result(this, 'events')))) return this;
@@ -63,34 +81,44 @@
           var method = events[key];
           if (!_.isFunction(method)) method = this[events[key]];
           if (!method) continue;
-
           var match = key.match(delegateEventSplitter);
-          var eventName = match[1], selector = match[2];
-          method = _.bind(method, this);
-          eventName += '.delegateEvents' + this.cid;
-
-          if (selector === '') {
-            this.$el.on(eventName, method);
-          } else {
-            this.$el.find(selector).each(function(child){
-              child.on(eventName, method);
-            });
-          }
+          this.delegate(match[1], match[2], _.bind(method, this));
         }
         return this;
+      },
+      delegate: function(eventName, selector, listener) {
+        // listener = selector || listener;
+        eventName += '.delegateEvents' + this.cid;
+        if (selector === '') {
+          this.el.on(eventName, listener);
+        } else if (!listener) {
+          this.el.on(eventName, selector);
+        } else {
+          this.el.find && this.el.find(selector).each(function(child){
+            child.on(eventName, listener);
+          });
+        }
       },
       _configure: function(options) {
         if (this.options) options = _.extend({}, _.result(this, 'options'), options);
         _.extend(this, _.pick(options, viewOptions));
         this.options = options;
       },
+      _createElement: function(nodeType) {
+        return new Kinetic[nodeType];
+      },
       _ensureElement: function() {
         if (!this.el) {
-          var $el = new Kinetic.Group();
-          this.setElement($el, false);
+          var attrs = _.extend({}, _.result(this, 'attributes'));
+          if (this.id) attrs.id = _.result(this, 'id');
+          this.setElement(this._createElement(_.result(this, 'nodeType')));
+          this._setAttributes(attrs);
         } else {
-          this.setElement(_.result(this, 'el'), false);
+          this.setElement(_.result(this, 'el'));
         }
+      },
+      _setAttributes: function(attributes) {
+        this.el.setAttrs(attributes);
       }
     });
     return KineticView;
--- test/kineticView.js
+++ test/kineticView.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
   var rect;
@@ -7,18 +8,29 @@ $(document).ready(function() {
   module("Backbone.View", {
 
     setup: function() {
-      group = new Kinetic.Group();
-      rect = new Kinetic.Rect({
-        id : "test"
+      // group = new Kinetic.Group();
+      // rect = new Kinetic.Rect({
+      //   id : "test"
+      // });
+      // group.add(rect);
+      view = new Backbone.KineticView({
+        id        : 'test-view',
+        other     : 'non-special-option'
       });
-      group.add(rect);
-      view = new Backbone.KineticView({el: group});
     }
 
   });
 
-  test("constructor", function() {
-    equal(view.$el instanceof Kinetic.Group, true);
+  test("constructor", 2, function() {
+    equal(view.el.id(), 'test-view');
+    equal(view.el.other, void 0);
+  });
+
+  test("el", 2, function() {
+    var view = new Backbone.KineticView();
+    strictEqual(view.el.nodeType, "Group");
+
+    ok(view.el instanceof Kinetic.Group);
   });
 
   test("initialize", 1, function() {
@@ -32,23 +44,53 @@ $(document).ready(function() {
   });
 
   test("delegateEvents", 3, function() {
+    var group = new Kinetic.Group();
+    var rect = new Kinetic.Rect({
+      id : "test"
+    });
+    group.add(rect);
+    var view = new Backbone.KineticView({
+      el : group
+    });
     var counter1 = 0;
-    view.increment = function(){ counter1++; };
 
-    var events = {'click #test': 'increment'};
+    view.increment = function() {
+        counter1++;
+    };
+    var events = {
+        'click #test': 'increment'
+    };
     view.delegateEvents(events);
-    rect.fire('click');
+
+    view.el.find('#test')[0].fire('click');
     equal(counter1, 1);
 
-    rect.fire('click');
+    view.el.find('#test')[0].fire('click');
     equal(counter1, 2);
 
     view.delegateEvents(events);
-    rect.fire('click');
+    view.el.find('#test')[0].fire('click');
     equal(counter1, 3);
   });
 
+  test("delegate", 2, function() {
+    var group = new Kinetic.Group();
+    var rect = new Kinetic.Rect();
+    group.add(rect);
+    var view = new Backbone.KineticView({
+      el : group
+    });
+    view.delegate('click', 'Rect', function() {
+      ok(true);
+    });
+    view.delegate('click', function() {
+      ok(true);
+    });
+    view.el.find('Rect')[0]._fireAndBubble('click', {});
+  });
+
   test("delegateEvents allows functions for callbacks", 3, function() {
+    var view = new Backbone.KineticView();
     view.counter = 0;
 
     var events = {
@@ -58,21 +100,22 @@ $(document).ready(function() {
     };
 
     view.delegateEvents(events);
-    view.$el.fire('click');
+    view.el.fire('click');
     equal(view.counter, 1);
 
-    view.$el.fire('click');
+    view.el.fire('click');
     equal(view.counter, 2);
 
     view.delegateEvents(events);
-    view.$el.fire('click');
+    view.el.fire('click');
     equal(view.counter, 3);
   });
 
 
   test("delegateEvents ignore undefined methods", 0, function() {
+    var view = new Backbone.KineticView();
     view.delegateEvents({'click': 'undefinedMethod'});
-    view.$el.fire('click');
+    view.el.fire('click');
   });
 
   test("undelegateEvents", 6, function() {
@@ -85,27 +128,111 @@ $(document).ready(function() {
 
     var counter1 = 0, counter2 = 0;
     view.increment = function(){ counter1++; };
-    view.$el.on('click', function(){
+    view.el.on('click', function(){
       counter2++;
     });
 
     var events = {'click .test': 'increment'};
 
     view.delegateEvents(events);
-    rect.fire('click', null, true);
+    rect._fireAndBubble('click', {});
     equal(counter1, 1);
     equal(counter2, 1);
 
     view.undelegateEvents();
-    rect.fire('click', null, true);
+    rect._fireAndBubble('click', {});
     equal(counter1, 1);
     equal(counter2, 2);
 
     view.delegateEvents(events);
-    rect.fire('click', null, true);
+    rect._fireAndBubble('click', {});
     equal(counter1, 2);
     equal(counter2, 3);
   });
+
+  test("undelegate", 0, function() {
+    var group = new Kinetic.Group();
+    var rect = new Kinetic.Rect({
+      name : "test"
+    });
+    group.add(rect);
+    view = new Backbone.KineticView({el: group});
+    view.delegate('click', function() { ok(false); });
+
+    view.undelegate('click');
+
+    view.el._fireAndBubble('click', {});
+  });
+
+  test("undelegate with selector", 2, function() {
+    var group = new Kinetic.Group();
+    var rect = new Kinetic.Rect({
+      name : "test"
+    });
+    group.add(rect);
+    view = new Backbone.KineticView({el: group});
+
+    view.delegate('click', function() {
+      ok(true); });
+    view.delegate('click', 'Rect', function() {
+      ok(false);
+    });
+
+    view.undelegate('click', 'Rect');
+    view.el.find("Rect")[0]._fireAndBubble('click', {});
+    view.el._fireAndBubble('click', {});
+  });
+
+  test("undelegate with handler and selector", 2, function() {
+   var group = new Kinetic.Group();
+    var rect = new Kinetic.Rect({
+      name : "test"
+    });
+    group.add(rect);
+    view = new Backbone.KineticView({el: group});
+    view.delegate('click', function() {ok(true); });
+    var handler = function(){ ok(false); };
+    view.delegate('click', 'Rect', handler);
+    view.undelegate('click', 'Rect', handler);
+    view.el.find('Rect')[0]._fireAndBubble('click', {});
+    view.el._fireAndBubble('click', {});
+  });
+
+  test("with nodeType and id functions", 2, function() {
+    var View = Backbone.KineticView.extend({
+      nodeType: function() {
+        return 'Rect';
+      },
+      id: function() {
+        return 'id';
+      }
+    });
+    ok(new View().el instanceof Kinetic.Rect);
+
+    strictEqual(new View().el.id(), 'id');
+  });
+
+  test("with attributes", 2, function() {
+    var View = Backbone.KineticView.extend({
+      attributes: {
+        id: 'id',
+      },
+      nodeType : 'Circle'
+    });
+    ok(new View().el instanceof Kinetic.Circle);
+    strictEqual(new View().el.id(), 'id');
+  });
+
+  test("with attributes as a function", 1, function() {
+    var View = Backbone.KineticView.extend({
+      attributes: function() {
+        return {'id': 'id'};
+      }
+    });
+
+    strictEqual(new View().el.id(), 'id');
+  });
+
 
   test("multiple views per element", 3, function() {
     var count = 0;
@@ -133,15 +260,29 @@ $(document).ready(function() {
     equal(5, count);
   });
 
-  test("setElement uses provided object.", 3, function() {
+  test("custom events", 2, function() {
+    var View = Backbone.KineticView.extend({
+      events: {
+        "fake$event": function() { ok(true); }
+      }
+    });
+
+    var view = new View;
+    view.el.fire('fake$event');
+    view.el.fire('fake$event');
+
+    view.el.off('fake$event');
+    view.el.fire('fake$event');
+  });
+
+  test("setElement uses provided object.", 2, function() {
     var el = new Kinetic.Group();
 
     var view = new Backbone.KineticView();
-    ok(view.$el !== el);
+    ok(view.el !== el);
 
     view.setElement(el);
-    ok(view.$el === el);
-    ok(view.el !== el);
+    ok(view.el === el);
   });
 
   test("Undelegate before changing element.", 1, function() {
@@ -151,7 +292,7 @@ $(document).ready(function() {
     var View = Backbone.KineticView.extend({
       events: {
         click: function(e) {
-          ok(view.$el === button2);
+          ok(view.el === button2);
         }
       }
     });
@@ -161,6 +302,72 @@ $(document).ready(function() {
 
     button1.fire('click');
     button2.fire('click');
-  })
+  });
+
+  test("views stopListening", 0, function() {
+    var View = Backbone.KineticView.extend({
+      initialize: function() {
+        this.listenTo(this.model, 'all x', function(){ ok(false); });
+        this.listenTo(this.collection, 'all x', function(){ ok(false); });
+      }
+    });
+
+    var view = new View({
+      model: new Backbone.Model,
+      collection: new Backbone.Collection
+    });
+
+    view.stopListening();
+    view.model.trigger('x');
+    view.collection.trigger('x');
+  });
+
+  test("Provide function for el.", 1, function() {
+    var View = Backbone.KineticView.extend({
+      el: function() {
+        return new Kinetic.Circle();
+      }
+    });
+
+    var view = new View;
+    ok(view.el instanceof Kinetic.Circle);
+  });
+
+  test("events passed in options", 1, function() {
+    var counter = 0;
+
+    var View = Backbone.KineticView.extend({
+      el: new Kinetic.Group().add(new Kinetic.Rect),
+      increment: function() {
+        counter++;
+      }
+    });
+
+    var view = new View({
+      events: {
+        'click Rect': 'increment'
+      }
+    });
+
+    view.el.find('Rect').fire('click');
+    view.el.find('Rect').fire('click');
+    equal(counter, 2);
+  });
+
+  test("remove", 1, function() {
+    var parent = new Kinetic.Group();
+    var view = new Backbone.KineticView;
+    parent.add(view.el);
+
+    view.delegate('click', function() { ok(false); });
+    view.listenTo(view, 'all x', function() { ok(false); });
+
+    view.remove();
+    view.el.fire('click');
+    view.trigger('x');
+
+    // In IE8 and below, parentNode still exists but is not document.body.
+    notEqual(view.el.parent, parent);
+  });
 
 });
